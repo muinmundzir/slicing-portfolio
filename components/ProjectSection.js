2fcc888624c5fab917e2e0285d418f9938b0c373
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2021-10-18 11:32:59 +0700

    add projects section

---
+++ components/ProjectSection.js
@@ -0,0 +1,46 @@
+import ProjectCard from './ProjectCard';
+import SectionParagraph from './SectionParagraph';
+import SectionTitle from './SectionTitle';
+
+const ProjectSection = () => {
+  return (
+    <section className="py-28">
+      <div className="container mx-auto">
+        <SectionTitle>Projects</SectionTitle>
+        <SectionParagraph>Beberapa proyek bikinan saya.</SectionParagraph>
+        <div className="flex flex-wrap -mx-4 mt-20">
+          <div className="w-6/12 px-4 pb-8">
+            <ProjectCard
+              image="/hasten.png"
+              description="Explorasi landing page"
+              name="Hasten"
+            />
+          </div>
+          <div className="w-6/12 px-4 pb-8">
+            <ProjectCard
+              image="/resource.png"
+              description="Explorasi landing page"
+              name="Resource"
+            />
+          </div>
+          <div className="w-6/12 px-4 pb-8">
+            <ProjectCard
+              image="/resource.png"
+              description="Explorasi landing page"
+              name="Resource"
+            />
+          </div>
+          <div className="w-6/12 px-4 pb-8">
+            <ProjectCard
+              image="/hasten.png"
+              description="Explorasi landing page"
+              name="Hasten"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ProjectSection;
